7be5d98aeede4645144c6846c58e4cdd74c24498
Small Long <910.long@gmail.com>
2024-03-28 21:33:40 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='png_to_gif_converter',
+    version='1.0.0',
+    description='Convert PNG images to GIF',
+    author='long-910',
+    author_email='910.long@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow>=8.0.0',
+    ],
+)
